631b7b1c2cd1d0397506eb343a0241741326aa18
ADITYA KUMAR GUPTA <30798959+aditya1996gupta@users.noreply.github.com>
2020-06-01 20:01:27 +0530

    Add files via upload
    app.vue for slots

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+<MyChildComponent></MyChildComponent>
+//Namedslots
+	<ArticleTemplate>
+		<template #header>
+			<h1>Page Header</h1>
+		</template>
+		<h3>VUE CLI</h3>
+		<p>Article Content</p>
+
+		<template #footer>
+			<p>Page Footer</p>
+		</template>
+	</ArticleTemplate>
+
+//Scoppedslots
+		<FruitList :fruits="fruitsOfTheNinja">
+			<template #default="{ fruit, slice }">
+				{{ fruit.name }}
+				<button @click="slice(fruit.id)">
+					⚔️ Style Slicing
+				</button>
+			</template>
+		</FruitList>
+	</div>
+</template>
+
+<script>
+import Slots from './components/Slots.vue'
+import Namedslots from './components/Namedslots.vue'
+import Scoppedslots from './components/Scoppedslots.vue'
+export default {
+	data () {
+		return {
+			fruitsOfTheNinja: [
+				{
+					id: 1,
+					name: '🍋Lemon'
+				},
+				{
+					id: 2,
+					name: '🥝Kiwi',
+					recipe: 'Kiwi, Baijiu & Red Date Yoghurt Cocktail 🍸'
+				},
+				{
+					id: 3,
+					name: '🍑Peach'
+				},
+				{
+					id: 4,
+					name: '🍌Banana'
+				},
+				{
+					id: 5,
+					name: '🍎Apple'
+				}
+			]
+		}
+	},
+	components: {
+		'MyChildComponent': Slots,
+		'ArticleTemplate': Namedslots,
+		'FruitList': Scoppedslots
+	}
+}
+</script>
